dd58003c723d7a9b41b81db421a84e8381094256
Seruy50 <Seruy50@ukr.net>
2023-02-10 19:06:05 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import style from './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+let questionsList = [
+  {
+    id: 1,
+    question: 'Провідне місто Волинської області',
+    answer: 'Горохів'
+  },
+  {
+    id: 2,
+    question: 'Важливий торгово-ремісничий населений пункт для історії Волині',
+    answer: 'Володимир'
+  },
+  {
+    id: 3,
+    question: 'Столиця це?',
+    answer: 'іменник'
+  },
+  {
+    id: 4,
+    question: 'Питань більше немає',
+    answer: 'іменник'
+  }
+]
+  let i = 1;
+
+export default function App(){
+  
+  const [value, setValue] = useState('');
+  const [currentQuestion, setCurrentQuestion] = useState(
+    questionsList.filter(item => {
+      if(+item.id === i){
+        return item;
+      }
+    })
+  )
+  const [points, setPoints] = useState(0);
+  const [answ, setAnsw] = useState('')
+  const [dis, setDis] = useState(false);
+
+  const buttonPress = () => {
+    if(value === currentQuestion[0].answer){
+      setAnsw(true);
+    } else {
+      setAnsw(false);
+    }
+    setDis(true);
+  }
+
+  const nextQuestion = () => {
+    if(i > 2){
+      setCurrentQuestion('Thats all questions for today')
+    }
+    i++;
+    setValue('');
+    setCurrentQuestion(
+      questionsList.filter(item => {
+        if(+item.id === i){
+          console.log(item.id)
+          return item;
+      }
+    }))
+    if(value === currentQuestion[0].answer){
+      setPoints(points + 1)
+    }
+    setAnsw('');
+    setDis(false);
+  }
+
+  return <div className="container">
+    <div className="questionField">
+      <p className="question">{currentQuestion[0].question}</p>
     </div>
-  );
+    <div className="answerAndPointsField">
+      <input value={value} onChange={(e) => setValue(e.target.value)} className="answerField" />
+      <button onClick={buttonPress} disabled={dis}>Collect answer</button>
+      <span className={answ === '' ? 'answerResume' : (answ ? 'answerResume right' :'answerResume wrong')}></span> 
+      <span className="points">{points}</span> 
+    </div>
+    <button onClick={nextQuestion} disabled={i > 3 ? true : false}>Next question</button>
+  </div>
 }
-
-export default App;
